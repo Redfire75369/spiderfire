3f5ddf2c3d5b381dc15f4645f7aeb1a77eca7f8d
sagudev <samo.golez@outlook.com>
2021-04-05 11:06:39 +0200

    Seperate tests

--- tests/main.rs
+++ tests/main.rs
@@ -8,10 +8,3 @@ fn main_js() {
 	CONFIG.set(config).unwrap();
 	run::run(&String::from("./tests/main.js"));
 }
-
-#[test]
-fn module_js() {
-	let config = Config::initialise(true, true).unwrap();
-	CONFIG.set(config).unwrap();
-	run::run(&String::from("./tests/module.js"));
-}
