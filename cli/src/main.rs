9ce9bd6f8690f0e01701635f64f968dacc4d870c
Vaimer9 <vaim3r9@gmail.com>
2021-11-24 11:47:05 +0530

    Final commit, cli rewritten in Structopt

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -4,8 +4,6 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
-use clap::{App, Arg, SubCommand};
-
 use runtime::config::{Config, CONFIG, LogLevel};
 use structopt::StructOpt;
 use crate::commands::{repl, run};
@@ -15,33 +13,31 @@ mod commands;
 pub mod evaluate;
 
 
-#[derive(StructOpt, Debug)]
-#[structopt(
-    name = "spiderfire",
-    about = "Javascript Runtime"
-)]
+#[derive(StructOpt)]
+#[structopt(name = "spiderfire", about = "JavaScript Runtime")]
 struct Cli {
+    #[structopt(subcommand)]
     commands: Option<Commands>
 }
 
 
 #[derive(StructOpt)]
 pub enum Commands {
-    #[structopt(about="Evaluated a line of Javascript")]
+    #[structopt(about="Evaluated a line of JavaScript")]
     Eval {
-        #[structopt(required(true), about="Line of Javascript to be evaluated")]
+        #[structopt(required(true), about="Line of JavaScript to be evaluated")]
         source: String
     },
 
-    #[structopt(about="Starts a Javascript Shell")]
+    #[structopt(about="Starts a JavaScript Shell")]
     Repl,
 
-    #[structopt(about="Runs a javascript file")]
+    #[structopt(about="Runs a javaScript file")]
     Run {
-        #[structopt(about="The Javascript file to run. Default: 'main.js'", required(false), default_value="main.js")]
+        #[structopt(about="The JavaScript file to run. Default: 'main.js'", required(false), default_value="main.js")]
         path: String,
 
-        #[structopt(about="Sets loggin level, Default: ERROR",required(false), default_value = "error")]
+        #[structopt(about="Sets loggin level, Default: ERROR", short, long, required(false), default_value = "error")]
         log_level: String,
 
         #[structopt(about="Sets logging level to DEBUG.", short)]
@@ -55,14 +51,13 @@ pub enum Commands {
 fn main() {
     let args = Cli::from_args();
 
-    println!("{:?}", args);
 
-    match args {
+    match args.commands {
         Some(Eval { source }) => {
-             //CONFIG
-				//.set(Config::default().log_level(LogLevel::Debug).script(true))
-				//.expect("Config Initialisation Failed");
-			//eval::eval_source(source);
+             CONFIG
+                .set(Config::default().log_level(LogLevel::Debug).script(true))
+                .expect("Config Initialisation Failed");
+            eval::eval_source(source);
             println!("{}", source);
         }
 
@@ -82,10 +77,10 @@ fn main() {
 				    _ => panic!("Invalid Logging Level")
                 }
             }
-			//CONFIG
-				//.set(Config::default().log_level(log_level).script(script))
-				//.expect("Config Initialisation Failed");
-			//run::run(path);
+            CONFIG
+                .set(Config::default().log_level(log_lev).script(script))
+                .expect("Config Initialisation Failed");
+            run::run(path);
 
             match log_lev {
                 LogLevel::None => println!("none"),
@@ -97,10 +92,10 @@ fn main() {
         }
 
         Some(Repl) | None => {
-            //CONFIG
-				//.set(Config::default().log_level(LogLevel::Debug).script(true))
-				//.expect("Config Initialisation Failed");
-			//repl::start_repl();
+            CONFIG
+                .set(Config::default().log_level(LogLevel::Debug).script(true))
+                .expect("Config Initialisation Failed");
+            repl::start_repl();
             println!("REPL!");
         }
 
