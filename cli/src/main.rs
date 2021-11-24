ec70818edc42aadeb1b3ca39b6f1952da802ab4d
Vaimer9 <vaim3r9@gmail.com>
2021-11-24 12:13:39 +0530

    did cargo fmt

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -12,81 +12,82 @@ use crate::commands::eval;
 mod commands;
 pub mod evaluate;
 
-
 #[derive(StructOpt)]
 #[structopt(name = "spiderfire", about = "JavaScript Runtime")]
 struct Cli {
-    #[structopt(subcommand)]
-    commands: Option<Commands>
+	#[structopt(subcommand)]
+	commands: Option<Commands>,
 }
 
 #[derive(StructOpt)]
 pub enum Commands {
-    #[structopt(about="Evaluates a line of JavaScript")]
-    Eval {
-        #[structopt(required(true), about="Line of JavaScript to be evaluated")]
-        source: String
-    },
+	#[structopt(about = "Evaluates a line of JavaScript")]
+	Eval {
+		#[structopt(required(true), about = "Line of JavaScript to be evaluated")]
+		source: String,
+	},
 
-    #[structopt(about="Starts a JavaScript Shell")]
-    Repl,
+	#[structopt(about = "Starts a JavaScript Shell")]
+	Repl,
 
-    #[structopt(about="Runs a JavaScript file")]
-    Run {
-        #[structopt(about="The JavaScript file to run. Default: 'main.js'", required(false), default_value="main.js")]
-        path: String,
+	#[structopt(about = "Runs a JavaScript file")]
+	Run {
+		#[structopt(about = "The JavaScript file to run. Default: 'main.js'", required(false), default_value = "main.js")]
+		path: String,
 
-        #[structopt(about="Sets logging level, Default: ERROR", short, long, required(false), default_value = "error")]
-        log_level: String,
+		#[structopt(about = "Sets logging level, Default: ERROR", short, long, required(false), default_value = "error")]
+		log_level: String,
 
-        #[structopt(about="Sets logging level to DEBUG.", short, long)]
-        debug: bool,
+		#[structopt(about = "Sets logging level to DEBUG.", short, long)]
+		debug: bool,
 
-        #[structopt(about="Disables ES Modules Features", short, long)]
-        script: bool
-    }
+		#[structopt(about = "Disables ES Modules Features", short, long)]
+		script: bool,
+	},
 }
 
 fn main() {
-    let args = Cli::from_args();
+	let args = Cli::from_args();
 
+	match args.commands {
+		Some(Eval { source }) => {
+			CONFIG
+				.set(Config::default().log_level(LogLevel::Debug).script(true))
+				.expect("Config Initialisation Failed");
+			eval::eval_source(source);
+		}
 
-    match args.commands {
-        Some(Eval { source }) => {
-             CONFIG
-                .set(Config::default().log_level(LogLevel::Debug).script(true))
-                .expect("Config Initialisation Failed");
-            eval::eval_source(source);
-        }
-
-        Some(Run { path, log_level, debug, script }) => {
+		Some(Run {
+			path,
+			log_level,
+			debug,
+			script,
+		}) => {
 			let mut log_lev = LogLevel::Error;
 
-            if debug {
+			if debug {
 				log_lev = LogLevel::Debug
-			} else  {
+			} else {
+				match log_level.to_uppercase().as_str() {
+					"NONE" => log_lev = LogLevel::None,
+					"INFO" => log_lev = LogLevel::Info,
+					"WARN" => log_lev = LogLevel::Warn,
+					"ERROR" => log_lev = LogLevel::Error,
+					"DEBUG" => log_lev = LogLevel::Debug,
+					_ => panic!("Invalid Logging Level"),
+				}
+			}
+			CONFIG
+				.set(Config::default().log_level(log_lev).script(script))
+				.expect("Config Initialisation Failed");
+			run::run(path);
+		}
 
-			    match log_level.to_uppercase().as_str() {
-                    "NONE" => log_lev = LogLevel::None,
-				    "INFO" => log_lev = LogLevel::Info,
-				    "WARN" => log_lev = LogLevel::Warn,
-				    "ERROR" => log_lev = LogLevel::Error,
-				    "DEBUG" => log_lev = LogLevel::Debug,
-				    _ => panic!("Invalid Logging Level")
-                }
-            }
-            CONFIG
-                .set(Config::default().log_level(log_lev).script(script))
-                .expect("Config Initialisation Failed");
-            run::run(path);
-        }
-
-        Some(Repl) | None => {
-            CONFIG
-                .set(Config::default().log_level(LogLevel::Debug).script(true))
-                .expect("Config Initialisation Failed");
-            repl::start_repl();
-        }
-    }
+		Some(Repl) | None => {
+			CONFIG
+				.set(Config::default().log_level(LogLevel::Debug).script(true))
+				.expect("Config Initialisation Failed");
+			repl::start_repl();
+		}
+	}
 }
-
