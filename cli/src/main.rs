baa837bb10db13aa6774102c32b5f8e3415b0ed2
Vaimer9 <vaim3r9@gmail.com>
2021-11-24 11:01:03 +0530

    rewrote cli  in structopt

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -7,102 +7,92 @@
 use clap::{App, Arg, SubCommand};
 
 use runtime::config::{Config, CONFIG, LogLevel};
-
+use structopt::StructOpt;
 use crate::commands::{repl, run};
 use crate::commands::eval;
 
 mod commands;
 pub mod evaluate;
 
+#[derive(StructOpt, Debug)]
+#[structopt(
+    name = "spiderfire",
+    about = "Javascript Runtime"
+)]
+pub enum Cli {
+    #[structopt(about="Evaluated a line of Javascript")]
+    Eval {
+        #[structopt(required(true), about="Line of Javascript to be evaluated")]
+        source: String
+    },
+
+    #[structopt(about="Starts a Javascript Shell")]
+    Repl,
+
+    #[structopt(about="Runs a javascript file")]
+    Run {
+        #[structopt(about="The Javascript file to run. Default: 'main.js'", required(false), default_value="main.js")]
+        path: String,
+
+        #[structopt(about="Sets loggin level, Default: ERROR",required(false))]
+        log_level: String,
+
+        #[structopt(about="Sets logging level to DEBUG.", short)]
+        debug: bool,
+
+        #[structopt(about="Disables ES Modules Features", short)]
+        script: bool
+    }
+}
+
 fn main() {
-	let matches = App::new("Spiderfire")
-		.version("0.1.0")
-		.about("JavaScript Runtime")
-		.subcommand(
-			SubCommand::with_name("eval")
-				.about("Evaluates a line of JavaScript")
-				.arg(Arg::with_name("source").help("Line of JavaScript to be evaluated").required(true)),
-		)
-		.subcommand(SubCommand::with_name("repl").about("Starts a JavaScript Shell"))
-		.subcommand(
-			SubCommand::with_name("run")
-				.about("Runs a JavaScript File")
-				.arg(
-					Arg::with_name("path")
-						.help("The JavaScript file to be run. Default: 'main.js'")
-						.required(false),
-				)
-				.arg(
-					Arg::with_name("log_level")
-						.help("Sets Logging Level. Default: ERROR")
-						.takes_value(true)
-						.short("l")
-						.long("loglevel")
-						.required(false)
-						.conflicts_with("debug"),
-				)
-				.arg(
-					Arg::with_name("debug")
-						.help("Sets Logging Level to DEBUG.")
-						.short("d")
-						.long("debug")
-						.required(false),
-				)
-				.arg(
-					Arg::with_name("script")
-						.help("Disables ES Modules Features")
-						.long("script")
-						.required(false),
-				),
-		)
-		.get_matches();
+    let args = Cli::from_args();
 
-	match matches.subcommand_name() {
-		Some("eval") => {
-			let subcmd = matches.subcommand_matches("eval").unwrap();
+    match args {
+        Eval { source } => {
+             //CONFIG
+				//.set(Config::default().log_level(LogLevel::Debug).script(true))
+				//.expect("Config Initialisation Failed");
+			//eval::eval_source(source);
+            println!("{}", source);
+        }
+        Repl | _ => {
+            //CONFIG
+				//.set(Config::default().log_level(LogLevel::Debug).script(true))
+				//.expect("Config Initialisation Failed");
+			//repl::start_repl();
+            println!("REPL!");
+        }
+        Run { path, log_level, debug, script } => {
+			let mut log_lev = LogLevel::Error;
 
-			CONFIG
-				.set(Config::default().log_level(LogLevel::Debug).script(true))
-				.expect("Config Initialisation Failed");
-			eval::eval_source(subcmd.value_of("source").unwrap());
-		}
-		Some("repl") => {
-			CONFIG
-				.set(Config::default().log_level(LogLevel::Debug).script(true))
-				.expect("Config Initialisation Failed");
-			repl::start_repl();
-		}
-		Some("run") => {
-			let subcmd = matches.subcommand_matches("run").unwrap();
+            if debug {
+				log_lev = LogLevel::Debug
+			} else  {
 
-			let log_level = if subcmd.is_present("debug") {
-				LogLevel::Debug
-			} else if let Some(level) = subcmd.value_of("log_level") {
-				let level = level.to_uppercase();
-				match level.as_str() {
-					"NONE" => LogLevel::None,
-					"INFO" => LogLevel::Info,
-					"WARN" => LogLevel::Warn,
-					"ERROR" => LogLevel::Error,
-					"DEBUG" => LogLevel::Debug,
-					_ => panic!("Invalid Logging Level"),
-				}
-			} else {
-				LogLevel::Error
-			};
+			    match log_level.to_uppercase().as_str() {
+                    "NONE" => log_lev = LogLevel::None,
+				    "INFO" => log_lev = LogLevel::Info,
+				    "WARN" => log_lev = LogLevel::Warn,
+				    "ERROR" => log_lev = LogLevel::Error,
+				    "DEBUG" => log_lev = LogLevel::Debug,
+				    _ => panic!("Invalid Logging Level")
+                }
+            }
+			//CONFIG
+				//.set(Config::default().log_level(log_level).script(script))
+				//.expect("Config Initialisation Failed");
+			//run::run(path);
 
-			CONFIG
-				.set(Config::default().log_level(log_level).script(subcmd.is_present("script")))
-				.expect("Config Initialisation Failed");
-			run::run(subcmd.value_of("path").unwrap_or("./main.js"));
-		}
+            match log_lev {
+                LogLevel::None => println!("none"),
+                LogLevel::Info => println!("info"),
+                LogLevel::Warn => println!("Warn"),
+                LogLevel::Error => println!("Error"),
+                LogLevel::Debug => println!("Debug")
+            }
+        }
 
-		None => {
-			CONFIG
-				.set(Config::default().log_level(LogLevel::Debug).script(true))
-				.expect("Config Initialisation Failed");
-			repl::start_repl();
-		}
-		_ => (),
-	}
+    }
+
 }
