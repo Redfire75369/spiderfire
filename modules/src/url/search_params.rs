17851837d7b2c938930e55958ab7560c5010e8d1
Redfire <redfire75369@hotmail.com>
2023-05-16 00:33:11 +0800

    Added URL.prototype.searchParams
    Switched URL and URLSearchParams to Use Reference-Counted URL

--- modules/src/url/search_params.rs
+++ modules/src/url/search_params.rs
@@ -8,62 +8,46 @@ pub use class::*;
 
 #[js_class]
 mod class {
-	use std::mem::transmute;
-	use mozjs::jsapi::{Heap, JSObject, JSTracer};
-	use mozjs::gc::Traceable;
-	use ion::{ClassInitialiser, Context, Error, ErrorKind, Object, Result};
-	use url::Url;
-	use crate::url::URL;
+	use std::cell::RefCell;
+	use std::rc::Rc;
 
-	#[ion(into_value)]
+	use url::Url;
+
+	#[ion(no_constructor, into_value)]
 	pub struct URLSearchParams {
-		object: Box<Heap<*mut JSObject>>,
-		url: &'static mut Url,
+		url: Rc<RefCell<Url>>,
 	}
 
 	// TODO: Allow URLSearchParams to be formed with just a string of query pairs
 	// TODO: Implement URLSearchParams.prototype.set and URLSearchParams.prototype.delete
+	// TODO: Implement [Symbol.iterator] for URLSearchParams
 	impl URLSearchParams {
-		#[allow(unused_mut)]
-		#[ion(constructor)]
-		pub fn constructor(cx: &Context, mut object: Object) -> Result<URLSearchParams> {
-			if URL::instance_of(cx, &object, None) {
-				let url = unsafe { transmute(URL::get_private(&mut object)) };
-				let object = Heap::boxed(**object);
-				Ok(URLSearchParams { object, url })
-			} else {
-				Err(Error::new("Expected URL", ErrorKind::Type))
-			}
+		pub(crate) fn from_url(url: Rc<RefCell<Url>>) -> URLSearchParams {
+			URLSearchParams { url }
 		}
 
 		pub fn append(&mut self, name: String, value: String) {
-			self.url.query_pairs_mut().append_pair(&name, &value);
+			self.url.borrow_mut().query_pairs_mut().append_pair(&name, &value);
 		}
 
 		pub fn get(&self, key: String) -> Option<String> {
-			self.url.query_pairs().into_owned().find(|(k, _)| k == &key).map(|(_, v)| v)
+			self.url.borrow().query_pairs().into_owned().find(|(k, _)| k == &key).map(|(_, v)| v)
 		}
 
 		pub fn getAll(&self, key: String) -> Vec<String> {
-			self.url.query_pairs().into_owned().filter(|(k, _)| k == &key).map(|(_, v)| v).collect()
+			self.url.borrow().query_pairs().into_owned().filter(|(k, _)| k == &key).map(|(_, v)| v).collect()
 		}
 
 		pub fn has(&self, key: String, value: Option<String>) -> bool {
 			if let Some(value) = value {
-				self.url.query_pairs().into_owned().any(|(k, v)| k == key && v == value)
+				self.url.borrow().query_pairs().into_owned().any(|(k, v)| k == key && v == value)
 			} else {
-				self.url.query_pairs().into_owned().any(|(k, _)| k == key)
+				self.url.borrow().query_pairs().into_owned().any(|(k, _)| k == key)
 			}
 		}
 
 		pub fn size(&self) -> i32 {
-			self.url.query_pairs().count() as i32
-		}
-	}
-
-	unsafe impl Traceable for URLSearchParams {
-		unsafe fn trace(&self, trc: *mut JSTracer) {
-			self.object.trace(trc);
+			self.url.borrow().query_pairs().count() as i32
 		}
 	}
 }
--- modules/src/url/url.rs
+++ modules/src/url/url.rs
@@ -7,36 +7,48 @@
 use idna::{domain_to_ascii, domain_to_ascii_strict, domain_to_unicode};
 use mozjs::jsapi::JSFunctionSpec;
 
+pub use class::*;
 use ion::{ClassInitialiser, Context, Object, Result};
 use runtime::modules::NativeModule;
 
-pub use class::*;
 use crate::url::search_params::URLSearchParams;
 
 #[js_class]
 mod class {
+	use std::cell::RefCell;
 	use std::cmp::Ordering;
+	use std::rc::Rc;
 
+	use ion::ClassInitialiser;
+
+	use crate::url::search_params::URLSearchParams;
 	use mozjs::conversions::ConversionBehavior::EnforceRange;
+	use mozjs::gc::Traceable;
+	use mozjs::jsapi::{Heap, JSObject, JSTracer};
 	use url::Url;
 
 	use ion::{Context, Error, Object, Result};
 
 	#[allow(clippy::upper_case_acronyms)]
-	#[derive(Clone)]
 	#[ion(from_value, to_value)]
 	pub struct URL {
-		url: Url,
+		url: Rc<RefCell<Url>>,
+		search_params: Box<Heap<*mut JSObject>>,
 	}
 
 	impl URL {
 		#[ion(constructor)]
-		pub fn constructor(input: String, base: Option<String>) -> Result<URL> {
+		pub fn constructor(cx: &Context, input: String, base: Option<String>) -> Result<URL> {
 			let options = Url::options();
 			let base = base.as_ref().and_then(|base| Url::parse(base).ok());
 			options.base_url(base.as_ref());
 			let url = options.parse(&input).map_err(|error| Error::new(&error.to_string(), None))?;
-			Ok(URL { url })
+			let url = Rc::new(RefCell::new(url));
+
+			let search_params = URLSearchParams::from_url(Rc::clone(&url));
+			let search_params = Heap::boxed(URLSearchParams::new_object(cx, search_params));
+
+			Ok(URL { url, search_params })
 		}
 
 		pub fn canParse(input: String, base: Option<String>) -> bool {
@@ -47,15 +59,15 @@ mod class {
 		}
 
 		pub fn toString(&self) -> String {
-			self.url.to_string()
+			self.url.borrow().to_string()
 		}
 
 		pub fn toJSON(&self) -> String {
-			self.url.to_string()
+			self.url.borrow().to_string()
 		}
 
 		pub fn format(&self, cx: &Context, options: Option<Object>) -> Result<String> {
-			let mut url = self.url.clone();
+			let mut url = self.url.borrow().clone();
 
 			let auth = options.as_ref().and_then(|options| options.get_as(cx, "auth", true, ())).unwrap_or(true);
 			let fragment = options
@@ -82,14 +94,16 @@ mod class {
 
 		#[ion(get)]
 		pub fn get_href(&self) -> String {
-			self.url.to_string()
+			self.url.borrow().to_string()
 		}
 
 		#[ion(set)]
-		pub fn set_href(&mut self, input: String) -> Result<()> {
+		pub fn set_href(&mut self, cx: &Context, input: String) -> Result<()> {
 			match Url::parse(&input) {
 				Ok(url) => {
-					self.url = url;
+					self.url = Rc::new(RefCell::new(url));
+					let search_params = URLSearchParams::from_url(Rc::clone(&self.url));
+					self.search_params = Heap::boxed(URLSearchParams::new_object(cx, search_params));
 					Ok(())
 				}
 				Err(error) => Err(Error::new(&error.to_string(), None)),
@@ -98,18 +112,18 @@ mod class {
 
 		#[ion(get)]
 		pub fn get_protocol(&self) -> String {
-			String::from(self.url.scheme())
+			String::from(self.url.borrow().scheme())
 		}
 
 		#[ion(set)]
 		pub fn set_protocol(&mut self, protocol: String) -> Result<()> {
-			self.url.set_scheme(&protocol).map_err(|_| Error::new("Invalid Protocol", None))
+			self.url.borrow_mut().set_scheme(&protocol).map_err(|_| Error::new("Invalid Protocol", None))
 		}
 
 		#[ion(get)]
 		pub fn get_host(&self) -> Option<String> {
-			self.url.host_str().map(|host| {
-				if let Some(port) = self.url.port() {
+			self.url.borrow().host_str().map(|host| {
+				if let Some(port) = self.url.borrow().port() {
 					format!("{}:{}", host, port)
 				} else {
 					String::from(host)
@@ -119,6 +133,7 @@ mod class {
 
 		#[ion(set)]
 		pub fn set_host(&mut self, host: Option<String>) -> Result<()> {
+			let mut url = self.url.borrow_mut();
 			if let Some(host) = host {
 				let segments: Vec<&str> = host.split(':').collect();
 				let (host, port) = match segments.len().cmp(&2) {
@@ -133,92 +148,113 @@ mod class {
 					}
 				}?;
 
-				self.url.set_host(Some(host))?;
+				url.set_host(Some(host))?;
 
-				self.url.set_port(port).map_err(|_| Error::new("Invalid URL", None))?;
+				url.set_port(port).map_err(|_| Error::new("Invalid URL", None))?;
 			} else {
-				self.url.set_host(None)?;
-				self.url.set_port(None).map_err(|_| Error::new("Invalid URL", None))?;
+				url.set_host(None)?;
+				url.set_port(None).map_err(|_| Error::new("Invalid URL", None))?;
 			}
 			Ok(())
 		}
 
 		#[ion(get)]
 		pub fn get_hostname(&self) -> Option<String> {
-			self.url.host_str().map(String::from)
+			self.url.borrow().host_str().map(String::from)
 		}
 
 		#[ion(set)]
 		pub fn set_hostname(&mut self, hostname: Option<String>) -> Result<()> {
-			self.url
+			self.url.borrow_mut()
 				.set_host(hostname.as_deref())
 				.map_err(|error| Error::new(&error.to_string(), None))
 		}
 
 		#[ion(get)]
 		pub fn origin(&self) -> String {
-			self.url.origin().ascii_serialization()
+			self.url.borrow().origin().ascii_serialization()
 		}
 
 		#[ion(get)]
 		pub fn get_port(&self) -> Option<u16> {
-			self.url.port_or_known_default()
+			self.url.borrow().port_or_known_default()
 		}
 
 		#[ion(set)]
 		pub fn set_port(&mut self, #[ion(convert = EnforceRange)] port: Option<u16>) -> Result<()> {
-			self.url.set_port(port).map_err(|_| Error::new("Invalid Port", None))
+			self.url.borrow_mut().set_port(port).map_err(|_| Error::new("Invalid Port", None))
 		}
 
 		#[ion(get)]
 		pub fn get_path(&self) -> String {
-			String::from(self.url.path())
+			String::from(self.url.borrow().path())
 		}
 
 		#[ion(set)]
 		pub fn set_path(&mut self, path: String) -> Result<()> {
-			self.url.set_path(&path);
+			self.url.borrow_mut().set_path(&path);
 			Ok(())
 		}
 
 		#[ion(get)]
 		pub fn get_username(&self) -> String {
-			String::from(self.url.username())
+			String::from(self.url.borrow().username())
 		}
 
 		#[ion(set)]
 		pub fn set_username(&mut self, username: String) -> Result<()> {
-			self.url.set_username(&username).map_err(|_| Error::new("Invalid URL", None))
+			self.url.borrow_mut().set_username(&username).map_err(|_| Error::new("Invalid URL", None))
 		}
 
 		#[ion(get)]
 		pub fn get_password(&self) -> Option<String> {
-			self.url.password().map(String::from)
+			self.url.borrow().password().map(String::from)
 		}
 
 		#[ion(set)]
 		pub fn set_password(&mut self, password: Option<String>) -> Result<()> {
-			self.url.set_password(password.as_deref()).map_err(|_| Error::new("Invalid URL", None))
+			self.url.borrow_mut().set_password(password.as_deref()).map_err(|_| Error::new("Invalid URL", None))
 		}
 
 		#[ion(get)]
 		pub fn get_search(&self) -> Option<String> {
-			self.url.query().map(String::from)
+			self.url.borrow().query().map(String::from)
 		}
 
 		#[ion(set)]
 		pub fn set_search(&mut self, search: Option<String>) {
-			self.url.set_query(search.as_deref());
+			self.url.borrow_mut().set_query(search.as_deref());
 		}
 
 		#[ion(get)]
 		pub fn get_hash(&self) -> Option<String> {
-			self.url.fragment().map(String::from)
+			self.url.borrow().fragment().map(String::from)
 		}
 
 		#[ion(set)]
 		pub fn set_hash(&mut self, hash: Option<String>) {
-			self.url.set_fragment(hash.as_deref());
+			self.url.borrow_mut().set_fragment(hash.as_deref());
+		}
+
+		#[ion(get)]
+		pub fn get_search_params(&self) -> *mut JSObject {
+			self.search_params.get()
+		}
+	}
+
+	impl Clone for URL {
+		fn clone(&self) -> URL {
+			URL {
+				url: Rc::clone(&self.url),
+				search_params: Heap::boxed(self.search_params.get())
+			}
+		}
+	}
+
+	unsafe impl Traceable for URL {
+		#[inline]
+		unsafe fn trace(&self, trc: *mut JSTracer) {
+			self.search_params.trace(trc);
 		}
 	}
 }
