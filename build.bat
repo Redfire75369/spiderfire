3f230ec7c8c2803b03ffbaa72f43267d3c7fe7a7
Redfire <redfire75369@hotmail.com>
2023-06-23 22:02:45 +0800

    Fixed Cargo Lockfile

--- build.bat
+++ build.bat
@@ -1,9 +1,9 @@
 @ECHO OFF
 
-set MOZTOOLS_PATH=C:\mozilla-build\msys\bin;C:\mozilla-build\bin
-set AUTOCONF=C:\mozilla-build\msys\local\bin\autoconf-2.13
-set NATIVE_WIN32_PYTHON=C:\mozilla-build\python\python2.7.exe
-set PYTHON3=C:\mozilla-build\python3\python3.exe
+set MOZTOOLS_PATH=C:\mozilla-build-4.0\msys\bin;C:\mozilla-build-4.0\bin
+set AUTOCONF=C:\mozilla-build-4.0\msys\local\bin\autoconf-2.13
+set NATIVE_WIN32_PYTHON=C:\mozilla-build-4.0\python\python2.7.exe
+set PYTHON3=C:\mozilla-build-4.0\python3\python3.exe
 
 set LIBCLANG_PATH=C:\Program Files\LLVM\lib
 set CC=clang-cl.exe
